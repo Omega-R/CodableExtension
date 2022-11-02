80846b709ca43a68364a8cecb543b3185ef0618d
ruslan.sanarkhin <ruslan.sanarkhin@omega-r.com>
2022-11-02 11:19:00 +0300

    Add date wrappers.

---
+++ Sources/CodableExtensions/TypeWrappers/DateWrappers/OptionalDate.swift
@@ -0,0 +1,30 @@
+//
+//  OptionalDate.swift
+//  
+//
+//  Created by Omega on 02.11.2022.
+//
+
+import Foundation
+
+@propertyWrapper
+struct OptionalDate: Decodable {
+    
+    let wrappedValue: Date?
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        guard let decodedString = try? container.decode(String.self) else {
+            wrappedValue = nil
+            return
+        }
+        
+        if decodedString.isEmpty {
+            wrappedValue = nil
+        } else if let date = DateFormat.date(from: decodedString) {
+            wrappedValue = date
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unsupported date format: `\(decoder.codingPath)`")
+        }
+    }
+}
