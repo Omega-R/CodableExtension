c9b7675d239b8712df0a1dc12aa7815aba123406
ruslan.sanarkhin <ruslan.sanarkhin@omega-r.com>
2022-11-03 17:08:35 +0300

    Add encoding keys

--- Sources/DecodableExtensions/TypeWrappers/JsonString.swift
+++ Sources/DecodableExtensions/TypeWrappers/JsonString.swift
@@ -5,21 +5,26 @@
 //  Created by Omega on 01.11.2022.
 //
 
-//import Foundation
-//
-//@propertyWrapper
-//public struct JsonString<T: Codable>: Codable {
-//    
-//    public let wrappedValue: T
-//    
-//    public init(from decoder: Decoder) throws {
-//        let container = try decoder.singleValueContainer()
-//        if let object = try? container.decode(T.self) {
-//            wrappedValue = object
-//        } else {
-//            let jsonString = try container.decode(String.self)
-//            let jsonData = Data(jsonString.utf8)
-//            wrappedValue = try JSONDecoder().decode(T.self, from: jsonData)
-//        }
-//    }
-//}
+import Foundation
+
+@propertyWrapper
+public struct JsonString<T: Codable>: Codable {
+    
+    public let wrappedValue: T
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let object = try? container.decode(T.self) {
+            wrappedValue = object
+        } else {
+            let jsonString = try container.decode(String.self)
+            let jsonData = Data(jsonString.utf8)
+            wrappedValue = try JSONDecoder().decode(T.self, from: jsonData)
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
+    }
+}
